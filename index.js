0be60cc2a01fb25148d6cced65ccb83446e8d20d
Dino Tahto <dinotahto.dev@gmail.com>
2020-08-30 12:45:03 +0200

    Added parsing search page

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const got = require("got")
+const cheerio = require("cheerio");
 
 const url = "https://www.olx.ba/pretraga?trazilica=";
 
-function searchItems (searchTerm){
-	return got(`${url}${searchTerm}`)
-		.then(data=>data.body)
-}
 
-searchItems("usb")
-	.then(response => console.log(response))
+async function getLinks(searchTerm){
+	const items = [];
+	const data = await got(`${url}${searchTerm}`);
+	const html = await data.body;
+	const $ = cheerio.load(html);
+	$('div.artikal:has(>a)').each(function(i,element){
+		$element = $(element);
+		$link = $element.find('a');
+		$title = $link.find('p')
+		$price = $element.find('div.datum span')
+		$date = $element.find('div.datum div.kada');
+		const item = {
+			title: `${$title.text()}`,
+			price: `${$price.text()}`,
+			dateAdded: `${$date.attr('title')}`,
+			link: `${$link.attr('href')}`
+		}
+		items.push(item);
+			
+		//console.log("===============")
+		//console.log($title.text());
+		//console.log($link.attr('href'));
+		//console.log($price.text());
+		//console.log($date.attr('title'));
+	});
+	console.log(items);
+
+};
+
+getLinks("ruksak");
