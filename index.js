0c18498e342c11806367b233ff5ffa2e9b726a78
Dino Tahto <dinotahto.dev@gmail.com>
2020-09-01 08:20:57 +0200

    add basic error handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const scraper = require("./scraper.js");
 const ejs = require("ejs");
+const fs = require("fs");
 
 app.set("view engine","ejs");
 app.use(express.static('public'));
@@ -13,11 +14,29 @@ app.get("/",(req,res)=>{
 
 
 app.get("/search", (req,res)=>{
-	
+		const parsedSearchTerm = req.query.searchTerm.replace(/ /g,"+");
+		const items = [];
+		if(req.query.searchTerm==''){
+			res.render("results",{data:undefined})
+		} else {
+		scraper.miniSearch(parsedSearchTerm)
+				.then(data=>res.render("results",{data:data}))
+		}
+})
+app.get("/test",(req,res)=>{
 	const parsedSearchTerm = req.query.searchTerm.replace(/ /g,"+");
 	const items = [];
 	scraper.miniSearch(parsedSearchTerm)
-			.then(data=>res.render("results",{data:data}))
+		.then((data)=>{
+			fileName = req.query.searchTerm + ".json"
+			data=JSON.stringify(data);
+			fs.writeFile(fileName,data,(err)=>{
+				if(err){
+					return console.log(err);
+				}
+				console.log("The file was saved! ["+ fileName+"]");
+			})
+		})
 
 })
 
