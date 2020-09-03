f8f0edb9d0872bf6d0b8ad23d2676a503fd53d3b
Dino Tahto <dinotahto.dev@gmail.com>
2020-09-03 17:25:53 +0200

    added comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const scraper = require("./scraper.js");
 const ejs = require("ejs");
 const fs = require("fs");
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:'true',limit: '50mb'}));
@@ -21,15 +21,16 @@ app.get("/search", (req,res)=>{
 	const parsedSearchTerm = req.query.searchTerm.replace(/ /g,"+");
 
 	if(req.query.dataType==='simple'){
-
+        // Checks if cache exists, and serves from cache
         if(simpleCache[parsedSearchTerm]){
             console.log("Serving from cache");
             res.render("simpleResults",{items:{data:simpleCache[parsedSearchTerm],saved:false}});
         } else {
-
+            // If user leaves search box empty
 		    if(req.query.searchTerm==''){
 		    	res.render("simpleResults",{data:undefined})
 		    } else  {
+                // Gets and serves data
 		    	scraper.simpleSearch(parsedSearchTerm)
 		    			.then((data)=>{
                             simpleCache[parsedSearchTerm]=data;
@@ -40,6 +41,7 @@ app.get("/search", (req,res)=>{
 
 	} else if (req.query.dataType==='full'){
 
+        // Checks if cache exists, and serves from cache
         if(fullCache[parsedSearchTerm]){
             console.log("Serving from cache");
 			res.render("fullResults",{items2:{data:fullCache[parsedSearchTerm],saved:false}});
@@ -47,19 +49,22 @@ app.get("/search", (req,res)=>{
 
 		    const items = [];
 		    const promises=[];
-
+            
+            // If user leaves search box empy
 		    if(req.query.searchTerm==''){
 		    	res.render("fullResults",{data:undefined})
 		    } else  {
-
+                // Gets IDs
 		        scraper.getIDS(parsedSearchTerm)
 		        		.then((ids)=>{
 		        				console.log(`Fetching data for ${ids.length} items`);
+                                // Gets data for each ID
 		        				ids.forEach(id=>{
 		        					promises.push(scraper.getData(id)
 		        								.then(data=>items.push(data)));
 		        								console.log(`Fetching data for [${id}]`)
 		        			})
+                            // Serve data once its fetched
 		        			Promise.all(promises).then(()=>{
 		        				console.log("Fetching completed. Displaying in browser...");
                                 fullCache[parsedSearchTerm]=items;
@@ -71,21 +76,21 @@ app.get("/search", (req,res)=>{
     }
 })
 
-
-
 app.post("/search/save",(req,res)=>{
+        // Writes data to data.json
 		fs.writeFile("data.json",req.body.data,(err)=>{
-			console.log("Data saved to [data.json]");
-			if(req.body.type==="full"){
-				res.render("fullResults",{items2:{data:JSON.parse(req.body.data),saved:true}});
-			}else {
-				res.render("simpleResults",{items:{data:JSON.parse(req.body.data),saved:true}});
-			}
-		if(err){
-			console.log(err);
-		}	
+		    if(err){
+		    	console.log(err);
+            } else {
+			    console.log("Data saved to [data.json]");
+                // Renders the page again, with already fetched data
+			    if(req.body.type==="full"){
+			    	res.render("fullResults",{items2:{data:JSON.parse(req.body.data),saved:true}});
+			    }else {
+			    	res.render("simpleResults",{items:{data:JSON.parse(req.body.data),saved:true}});
+			    }
+		    }	
 		})
-
 })
 
 const port = process.env.PORT || 3000;
--- scraper.js
+++ scraper.js
@@ -3,25 +3,26 @@ const cheerio = require("cheerio");
 
 const baseURL = "https://www.olx.ba/pretraga?trazilica=";
 
-
+// Gets data that is available on the results page
 async function simpleSearch(searchTerm){
-
 
 	const results = [];
 	let page = 1;
 	let search = true;
-
+    // Searches every page as long as there are results on the page
 	while(search==true){
 
+        // Loads the html into cheerio
 		const fullURL = `${baseURL}${searchTerm}&stranica=${page}`;
 		const data = await got(`${fullURL}`);
 		const html = await data.body;
 		const $ = cheerio.load(html);
 
+        // Checks if there is content on the page
 		if($('div.artikal:has(>a)').length){
 
 			console.log(`Scraping '${searchTerm.replace(/\+/g," ")}' on page ${page}`);
-
+            // Saves data from every element into array
 			$('div.artikal:has(>a)').each((i,element)=>{
 
 				$element = $(element);
@@ -39,9 +40,11 @@ async function simpleSearch(searchTerm){
 				}
 				results.push(result);
 			});
+            // Go to next page
 			page++;
 
 		} else {
+            // If there is not content on the page, stop
 			console.log(`Successfully scraped ${results.length} items.`); 
 			search=false;
 		}
@@ -49,23 +52,23 @@ async function simpleSearch(searchTerm){
 	return results;
 };
 
-
+// Gets the OLX ID for every item
 async function getIDS (searchTerm) {
-
 
 	let page=1;
 	console.log("Retrieving item IDs");
 	const itemIDS = [];
 	
 	let search=true;
-
+    // Searches every page that has content
 	while(search===true){
 
+        // Loads the html into cheerio
 		const fullURL = `${baseURL}${searchTerm}&stranica=${page}`;
 		const data = await got(`${fullURL}`);
 		const html = await data.body;
 		const $ = cheerio.load(html);
-
+            // Cheks if the page has content
 			if($('div.artikal:has(>a)').length){
 				console.log(`Scraping page ${page}`)
 				$('div.artikal:has(>a)').each(function(i,element){
@@ -83,13 +86,16 @@ async function getIDS (searchTerm) {
 	return itemIDS;
 }
 
+// Gets data for a given ID
 async function getData (id){
 
 	const fullURL = `https://www.olx.ba/artikal/${id}`;
 	const data = await got(`${fullURL}`);
 	const html = await data.body;
 	const $ = cheerio.load(html);
+
 	let itemData={};
+
 	try{
 		console.log(`Parsing data for [${id}]`)
 		const title = $('#naslovartikla');
@@ -110,7 +116,6 @@ async function getData (id){
 			}
 			itemData[$title]=`${$value}`;
 		})
-		
 
 	} catch(err){
 		console.log(err);
